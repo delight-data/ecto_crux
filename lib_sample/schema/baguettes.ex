7138904aaee0ad69648576d11bc999d1e2203ad0
Maxime Burriez <maxime.burriez@derniercri.io>
2020-10-27 14:30:19 +0100

    Add documentation for the require EctoCrux.Repo into EctoCrux.Schema.Baguettes

--- lib_sample/schema/baguettes.ex
+++ lib_sample/schema/baguettes.ex
@@ -11,5 +11,9 @@ defmodule EctoCrux.Schema.Baguettes do
 
   use EctoCrux, module: EctoCrux.Schema.Baguette
 
+  # To ensure that EctoCrux.Repo is compiled before EctoCrux.Schema.Baguettes.
+  # It is therefore specific to the Baguettes example integrated in Crux.
+  # Without this line, the compilation of EctoCrux.Repo comes too late since
+  # here we are not really in the global context of a Phoenix project.
   require EctoCrux.Repo
 end
