2087d2cf728a57c218cbd797dcb03d1b7f90ef25
Xavier Demompion <xwar_srkn@hotmail.com>
2022-01-12 15:36:55 +0100

    release v1.2.10

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule EctoCrux.MixProject do
     [
       app: :ecto_crux,
       name: "EctoCrux",
-      version: "1.2.9",
+      version: "1.2.10",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
