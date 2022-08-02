70e2bdf3733938b5ecae698ae17d48285b4b6931
Maksymilian Jodłowski <maksymilian.jodlowski@curiosum.com>
2022-08-02 10:23:25 +0200

    Add dialyxir to project

--- mix.exs
+++ mix.exs
@@ -33,7 +33,8 @@ defmodule EctoCrux.MixProject do
   defp deps do
     [
       {:ex_doc, ">= 0.0.0", only: :dev},
-      {:ecto_sql, "~> 3.5", only: [:dev, :test]}
+      {:ecto_sql, "~> 3.5", only: [:dev, :test]},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 
